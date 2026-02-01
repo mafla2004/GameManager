489784351548d6a0b3b1567e4f2fafca359b6311
Mafletto - Now doing computer bullshit professionally <mafla.caridi@gmail.com>
2026-02-01 19:54:03 +0100

    Adding activities and modifying values

---
+++ app/src/main/java/com/example/gamemanager/Project.kt
@@ -0,0 +1,9 @@
+package com.example.gamemanager
+
+class Project(private var Name: String = "New Project", private var Description: String = "Project Description")
+{
+    init
+    {
+
+    }
+}
---
+++ app/src/main/java/com/example/gamemanager/ProjectManagerActivity.kt
@@ -0,0 +1,23 @@
+package com.example.gamemanager
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+/*
+*   This is the activity in which the user will be able to see and manage their project.
+*/
+class ProjectManagerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_project_manager)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
